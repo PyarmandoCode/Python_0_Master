089f77f7368159fd2da68a21ff81b3a715ead969
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2024-04-23 22:23:28 -0500

    Clase04

---
+++ 100PY_CLASE_03/facturacion_poo_tkinter.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class Producto:
+    def __init__(self,nombre,precio,cantidad):
+        self.nombre=nombre
+        self.precio=precio
+        self.cantidad=cantidad
+
+    def calcular_total(self):
+        return self.precio*self.cantidad
+
+class FacturaApp:
+    def __init__(self,ventana):
+        self.ventana=ventana
+        self.ventana.title("Facturacion")        
+        self.productos=[]
+
+        self.lbl_nombre=tk.Label(ventana,text="Nombre del producto")
+        self.lbl_nombre.grid(row=0,column=0,padx=5,pady=5)
+        self.entry_nombre = tk.Entry(ventana)
+        self.entry_nombre.grid(row=0,column=1,padx=5,pady=5)
+
+        self.lbl_precio=tk.Label(ventana,text="Precio")
+        self.lbl_precio.grid(row=1,column=0,padx=5,pady=5)
+        self.entry_precio=tk.Entry(ventana)
+        self.entry_precio.grid(row=1,column=1,padx=5,pady=5)
+
+        self.lbl_cantidad=tk.Label(ventana,text="Cantidad")
+        self.lbl_cantidad.grid(row=2,column=0,padx=5,pady=5)
+        self.entry_cantidad=tk.Entry(ventana)
+        self.entry_cantidad.grid(row=2,column=1,padx=5,pady=5)
+
+        self.btn_agregar=tk.Button(ventana,text="Agregar Producto",command=self.agregar_producto)
+        self.btn_agregar.grid(row=3,column=0,columnspan=2,padx=5,pady=5)
+
+        self.btn_factura=tk.Button(ventana,text="Facturar",command=self.generar_factura)
+        self.btn_factura.grid(row=4,column=0,columnspan=2,padx=5,pady=5)
+
+        self.lbl_factura=tk.Label(ventana,text="")
+        self.lbl_factura.grid(row=5,column=0,columnspan=2,padx=5,pady=5)
+
+    def agregar_producto(self):
+        nombre=self.entry_nombre.get()
+        precio=float(self.entry_precio.get())
+        cantidad=int(self.entry_cantidad.get())
+
+        producto=Producto(nombre,precio,cantidad)
+        self.productos.append(producto)
+        messagebox.showinfo("Producto Agregado","Producto Agregado Correctamente")
+
+        self.entry_nombre.delete(0,tk.END)
+        self.entry_precio.delete(0,tk.END)
+        self.entry_cantidad.delete(0,tk.END)
+
+    def generar_factura(self):
+        #Hallo el total general de la factura
+        total=sum(producto.calcular_total() for producto in self.productos)    
+        #Para visualizacion o impresion
+        factura="Factura:\n\n"
+        for producto in self.productos:
+            factura += f"{producto.nombre}:${producto.precio} x {producto.cantidad} = ${producto.calcular_total()}\n"
+        factura +="\nTotal a Pagar:$" + str(total)
+        self.lbl_factura.config(text=factura)
+
+#Crear la ventana principal
+ventana_principal=tk.Tk()
+app = FacturaApp(ventana_principal)        
+ventana_principal.mainloop()
--- 100PY_CLASE_03/interfaces.py
+++ 100PY_CLASE_03/interfaces.py
@@ -4,6 +4,7 @@ PROGRAMACION FUNCIONAL
 INTERFACES TKINTER
 """
 import tkinter as tk
+from tkinter import messagebox
 
 def agregar_tarea(entrada,lista):
     tarea=entrada.get()
@@ -11,33 +12,56 @@ def agregar_tarea(entrada,lista):
         lista.insert(tk.END,tarea)
         entrada.delete(0,tk.END)
 
+def eliminar_tarea(lista):
+    tareas_seleccionada=lista.curselection()
+    if tareas_seleccionada:
+        lista.delete(tareas_seleccionada)    
+
+def salir(ventana):
+    if messagebox.askokcancel("Salir","¿Estas seguro que quieres salir"):
+        ventana.destroy()             
+
 def principal():
     #Crear la ventana principal
     ventana=tk.Tk() #creando la ventana
     ventana.title("Lista de Tareas")
+
+
     #Creando los Widgets
     marco_lista=tk.Frame(ventana)
     marco_lista.pack(pady=10)
+
     #Widget de lista de Tareas
     lista_tareas=tk.Listbox(marco_lista,width=50,height=10)
     lista_tareas.pack(side=tk.LEFT,fill=tk.BOTH)
+
     #Widget de scrollbar
     scrollbar=tk.Scrollbar(marco_lista,orient=tk.VERTICAL)
     scrollbar.config(command=lista_tareas.yview)
     scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
     lista_tareas.config(yscrollcommand=scrollbar.set)
+
     #Marco de entrada para la nueva Tarea
     marco_entrada=tk.Frame(ventana)
     marco_entrada.pack(pady=5)
+
     #Creamos la etiqueta y la entrada
     etiqueta_tarea=tk.Label(marco_entrada,text="Nueva Tarea")
     etiqueta_tarea.pack(side=tk.LEFT)
     entrada_tarea=tk.Entry(marco_entrada,width=40)
     entrada_tarea.pack(side=tk.LEFT)
+
     #Creamos el Boton
     boton_agregar=tk.Button(ventana,text="Agregar Tarea",command=lambda :agregar_tarea(entrada_tarea,lista_tareas))
     boton_agregar.pack(pady=5)
 
+    #Creamos un Boton para eliminar la tarea
+    boton_eliminar=tk.Button(ventana,text="Eliminar Tarea" ,command=lambda:eliminar_tarea(lista_tareas))
+    boton_eliminar.pack(pady=5)
+
+    #Creamos un Boton para cerrar la interfaz(GUI)
+    boton_salir=tk.Button(ventana,text="Salir",command=lambda:salir(ventana))
+    boton_salir.pack(pady=5)
     ventana.mainloop()
 
 if __name__=="__main__":
